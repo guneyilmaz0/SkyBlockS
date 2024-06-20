a7408afeea8a8f95d035f001b8afdc78563e363a
guney <guneyyilmaz2707@gmail.com>
2024-06-20 13:35:19 +0300

    improve ProtectionListener

--- src/main/kotlin/net/guneyilmaz0/skyblocks/listeners/ProtectionListener.kt
+++ src/main/kotlin/net/guneyilmaz0/skyblocks/listeners/ProtectionListener.kt
@@ -3,7 +3,6 @@ package net.guneyilmaz0.skyblocks.listeners
 import cn.nukkit.Player
 import cn.nukkit.event.Event
 import cn.nukkit.event.EventHandler
-import cn.nukkit.event.EventPriority
 import cn.nukkit.event.Listener
 import cn.nukkit.event.block.BlockBreakEvent
 import cn.nukkit.event.block.BlockPlaceEvent
@@ -13,27 +12,19 @@ import net.guneyilmaz0.skyblocks.objects.IslandData
 
 @Suppress("unused")
 class ProtectionListener : Listener {
-    @EventHandler(priority = EventPriority.LOW)
-    fun onBreakBlock(event: BlockBreakEvent) {
-        handleEvent(event.player, event)
-    }
 
-    @EventHandler(priority = EventPriority.LOW)
-    fun onPlaceBlock(event: BlockPlaceEvent) {
-        handleEvent(event.player, event)
-    }
+    @EventHandler
+    fun onBlockBreak(event: BlockBreakEvent) = handleEvent(event.player, event)
 
-    @EventHandler(priority = EventPriority.LOW)
-    fun onTouch(event: PlayerInteractEvent) {
-        handleEvent(event.player, event)
-    }
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) = handleEvent(event.player, event)
+
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) = handleEvent(event.player, event)
 
     private fun handleEvent(player: Player, event: Event) {
-        if (event.isCancelled || player.isOp || !IslandData.isIslandExists(player.getLevel().folderName)) return
-
-        val island: Island = Island.get(player.getLevel().folderName)
-        if (island.isMember(player.name) || player.hasPermission("skyblocks.touch.island"))
-            event.isCancelled = false
-        else event.setCancelled()
+        if (!event.isCancelled && !player.isOp && IslandData.isIslandExists(player.level.folderName))
+            event.isCancelled = !Island.get(player.level.folderName)
+                .isMember(player.name) && !player.hasPermission("skyblocks.touch.island")
     }
 }
