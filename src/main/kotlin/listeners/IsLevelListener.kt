fc66ab3dee1885e0bf601c74b6895e83c84e8a34
guneyilmaz0 <guneyyilmaz2707@gmail.com>
2025-01-12 02:43:08 +0300

    level system

--- src/main/kotlin/listeners/IsLevelListener.kt
+++ src/main/kotlin/listeners/IsLevelListener.kt
@@ -5,61 +5,25 @@ import cn.nukkit.event.EventHandler
 import cn.nukkit.event.EventPriority
 import cn.nukkit.event.Listener
 import cn.nukkit.event.block.BlockBreakEvent
-import cn.nukkit.event.block.BlockEvent
 import cn.nukkit.event.block.BlockPlaceEvent
-import cn.nukkit.level.Sound
-import cn.nukkit.level.particle.HappyVillagerParticle
-import cn.nukkit.scheduler.NukkitRunnable
 import net.guneyilmaz0.skyblocks.SkyBlockS
-import net.guneyilmaz0.skyblocks.island.Island
-import net.guneyilmaz0.skyblocks.utils.Translator.translate
+import net.guneyilmaz0.skyblocks.tasks.IslandXPTask
 
 @Suppress("unused")
 class IsLevelListener : Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
-    fun onBlockBreak(event: BlockBreakEvent) = addXpAsync(event, -1)
-
-    @EventHandler(priority = EventPriority.NORMAL)
-    fun onBlockPlace(event: BlockPlaceEvent) = addXpAsync(event, 1)
-
-    private fun addXpAsync(event: BlockEvent, amount: Int) {
+    fun onBlockBreak(event: BlockBreakEvent) {
         if (event.isCancelled) return
         if (!SkyBlockS.provider.isIslandExists(event.block.level.folderName)) return
-
-        val level = event.block.level
-        val island = Island.get(level.folderName)
-
-        Server.getInstance().scheduler.scheduleDelayedTask(SkyBlockS.instance, object : NukkitRunnable() {
-            override fun run() {
-                val i = island.database.level.increaseXp(amount)
-                when (i) {
-                    0 -> return
-                    1 -> {
-                        level.addParticle(HappyVillagerParticle(event.block))
-                        level.addSound(event.block, Sound.RANDOM_LEVELUP, 1f, 1f, island.getOnlineMembers())
-                        island.getOnlineMembers().forEach {
-                            it.sendMessage(
-                                translate(
-                                    it, "island.level.up",
-                                    island.database.level.level.toString()
-                                )
-                            )
-                        }
-                    }
-                    else -> {
-                        level.addSound(event.block, Sound.RANDOM_ANVIL_LAND, 1f, 1f, island.getOnlineMembers())
-                        island.getOnlineMembers().forEach {
-                            it.sendMessage(
-                                translate(
-                                    it, "island.level.down",
-                                    island.database.level.level.toString()
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-        }, 5, true)
+        Server.getInstance().scheduler.scheduleAsyncTask(SkyBlockS.instance, IslandXPTask(event.block.level, 1))
     }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        if (event.isCancelled) return
+        if (!SkyBlockS.provider.isIslandExists(event.block.level.folderName)) return
+        Server.getInstance().scheduler.scheduleAsyncTask(SkyBlockS.instance, IslandXPTask(event.block.level, 1))
+    }
+
 }
